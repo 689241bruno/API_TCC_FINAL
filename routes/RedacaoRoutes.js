499aab99308dc3533c7687a55db6e3d9bb37376e
Miguel Soares <mgsousa23lv@gmail.com>
2025-12-06 20:25:37 -0300

    Adição dos métodos e rotas da redação

--- routes/RedacaoRoutes.js
+++ routes/RedacaoRoutes.js
@@ -2,6 +2,28 @@ const express = require("express");
 const router = express.Router();
 const RedacaoController = require("../controllers/RedacaoController");
 
+router.get("/", (req,res) => {
+    const routes = [];
+    router.stack.forEach((middleware) => {
+        if (middleware.route) {
+        const path = middleware.route.path;
+        const method = Object.keys(middleware.route.methods).join(", ").toUpperCase();
+        routes.push({ path, method });
+        }
+    });
+
+    res.json({
+        message: "Raiz da API",
+        availableRoutes: routes,
+    });
+
+});
+
+router.get("/aluno/:id", RedacaoController.getRedacaoByAlunoID);
+router.get("/:id", RedacaoController.getRedacaoByID)
+
+router.post("/salvar", RedacaoController.salvar);
+
 // 🟦 Redações do aluno
 router.get("/aluno/:usuario_id", RedacaoController.listarRedacoes);
 
