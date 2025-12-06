da90cbbc20e501458b7f78f8c1fd5eaaa0acc22d
Bruno S. Santos <689241bruno@gmail.com>
2025-12-06 20:16:07 -0300

    upload cloudinary 10

--- server.js
+++ server.js
@@ -21,9 +21,6 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "API funcionando" });
 });
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
-
 // Aumenta o limite de payload para JSON e form-data
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
